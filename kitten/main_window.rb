73e57f3d5bb08ce3b0070258859df159bf614c84
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-05 22:35:26 +0200

    Updated main window to switch branches in history view.

--- kitten/main_window.rb
+++ kitten/main_window.rb
@@ -1,6 +1,7 @@
 
 require 'korundum4'
 
+require 'kitten/git_branches_model'
 require 'kitten/git_history_model'
 require 'kitten/ui_main_window'
 
@@ -9,7 +10,8 @@ require 'git'
 
 module Kitten
   class MainWindow < KDE::MainWindow
-      slots 'on_action_Open_triggered()'
+      slots 'on_action_Open_triggered()',
+            'on_historyBranchComboBox_currentIndexChanged(const QString&)'
 
       def initialize(*args)
         super
@@ -18,13 +20,23 @@ module Kitten
       end
 
       def loadModels()
-        @ui.historyTableView.model = GitHistoryModel.new(@repository, self)
+        @history_model = GitHistoryModel.new(@repository, self)
+        @branches_model = GitBranchesModel.new(@repository, self)
+
+        @ui.historyTableView.model = @history_model
+        @ui.historyBranchComboBox.model = @branches_model
+        currentBranchIndex = @ui.historyBranchComboBox.findText(@repository.current_branch)
+        @ui.historyBranchComboBox.currentIndex = currentBranchIndex
       end
 
       def on_action_Open_triggered()
         path = Qt::FileDialog.getExistingDirectory(self, "Select Git repository location")
         @repository = Git.open(path)
         loadModels()
+      end
+
+      def on_historyBranchComboBox_currentIndexChanged(currentBranch)
+        @history_model.setBranch(currentBranch)
       end
   end
 end
