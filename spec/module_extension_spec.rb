2971f3f4fb87a7e1973f34d0d7a35c5fa8c31ce6
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-09 01:08:53 +0200

    Started spec for Module extension.

---
+++ spec/module_extension_spec.rb
@@ -0,0 +1,27 @@
+
+require 'core_ext/module'
+
+describe Module, "attributes extionsion" do
+  it "should overwrite attr_writer" do
+    Module.private_methods.should include('attr_writer_with_camelize')
+    Module.private_methods.should include('attr_writer_without_camelize')
+  end
+
+  it "should generate setter methods" do
+    self.should_not respond_to('foo=')
+    class Foo
+      attr_writer :foo
+    end
+    a = Foo.new
+    a.should respond_to('foo=')
+  end
+
+  it "should generate setter methods respecting camelcased equivalents" do
+    self.should_not respond_to('foo=')
+    class Foo
+      attr_writer :foo
+    end
+    a = Foo.new
+    a.should respond_to('foo=')
+  end
+end
