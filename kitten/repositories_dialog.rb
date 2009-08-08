e4dd6a7d0bdd61e86ccf9e1a10acc475fb81832d
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-09 01:05:15 +0200

    Created a simple repository dialog class.

---
+++ kitten/repositories_dialog.rb
@@ -0,0 +1,22 @@
+
+require 'kitten/ui_repositories_dialog'
+
+require 'rubygems'
+require 'git'
+
+module Kitten
+  class RepositoriesDialog < KDE::Dialog
+
+      def initialize(*args)
+        super
+        @ui = Ui_RepositoriesDialog.new
+        @ui.setup_ui(self)
+
+        @ui.repositorySearchLine.list_widget = @ui.repositoriesListWidget
+      end
+
+      def selectedRepositoryPath()
+        @ui.repositoriesListWidget.current_item.text
+      end
+  end
+end
