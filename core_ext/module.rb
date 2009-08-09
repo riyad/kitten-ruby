76a737b522b2a758590d7690e9c8381a6f22f881
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-09 21:00:23 +0200

    Implemented attribute reader Module extension.

--- core_ext/module.rb
+++ core_ext/module.rb
@@ -7,6 +7,10 @@ module ModuleExtension
     base.send :alias_method, :attr_accessor_without_camelize, :attr_accessor
     base.send :alias_method, :attr_accessor, :attr_accessor_with_camelize
 
+    # attr_reader
+    base.send :alias_method, :attr_reader_without_camelize, :attr_reader
+    base.send :alias_method, :attr_reader, :attr_reader_with_camelize
+
     # attr_writer
     base.send :alias_method, :attr_writer_without_camelize, :attr_writer
     base.send :alias_method, :attr_writer, :attr_writer_with_camelize
@@ -17,6 +21,37 @@ module ModuleExtension
   def attr_accessor_with_camelize(*symbols)
     attr_reader(*symbols)
     attr_writer(*symbols)
+  end
+
+  def attr_reader_with_camelize(*symbols)
+    # generates the attribute writers
+    symbols.each do |sym|
+      attr_name = sym.id2name
+      method_name = attr_name
+
+      camelized_method_names = [] # readers can be ambiguous
+      [method_name, "#{method_name}?"].each do |variant|
+        camelized_method_names += camelize_method_name(variant)
+      end
+      camelized_method_names.reject! { |camelized_method_name| camelized_method_name == method_name }
+
+      p "#{method_name}: #{camelized_method_names.inspect}"
+
+      # generates the method with customized behaviour only if the name differs
+      # if there is a fooBarBaz/isFooBarBaz/hasFooBarBaz method it will be called
+      # otherwise standard behaviour is ensured
+      module_eval <<-END
+        def #{method_name}()
+          #{camelized_method_names.inspect}.each do |camelized_method_name|
+            if respond_to? camelized_method_name
+              return send camelized_method_name.to_sym
+            end
+          end
+
+          @#{attr_name}
+        end
+      END
+    end
   end
 
   def attr_writer_with_camelize(*symbols)
