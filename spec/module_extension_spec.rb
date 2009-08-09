daf6ec178f316f5d769b72913d3d0998e0196463
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-09 19:24:14 +0200

    Reorganized Module spec.

--- spec/module_extension_spec.rb
+++ spec/module_extension_spec.rb
@@ -1,27 +1,29 @@
 
 require 'core_ext/module'
 
-describe Module, "attributes extionsion" do
-  it "should overwrite attr_writer" do
-    Module.private_methods.should include('attr_writer_with_camelize')
-    Module.private_methods.should include('attr_writer_without_camelize')
-  end
-
-  it "should generate setter methods" do
-    self.should_not respond_to('foo=')
-    class Foo
-      attr_writer :foo
+describe Module, "attribute" do
+  context "writer extionsion" do
+    it "should overwrite attr_writer" do
+      Module.private_methods.should include('attr_writer_with_camelize')
+      Module.private_methods.should include('attr_writer_without_camelize')
     end
-    a = Foo.new
-    a.should respond_to('foo=')
-  end
 
-  it "should generate setter methods respecting camelcased equivalents" do
-    self.should_not respond_to('foo=')
-    class Foo
-      attr_writer :foo
+    it "should generate setter methods" do
+      self.should_not respond_to(:foo=)
+      class Foo
+        attr_writer :foo
+      end
+      a = Foo.new
+      a.should respond_to(:foo=)
     end
-    a = Foo.new
-    a.should respond_to('foo=')
+
+    it "should generate setter methods respecting camelcased equivalents" do
+      self.should_not respond_to(:foo=)
+      class Foo
+        attr_writer :foo
+      end
+      a = Foo.new
+      a.should respond_to(:foo=)
+    end
   end
 end
