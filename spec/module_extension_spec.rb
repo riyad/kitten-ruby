bbdd13c5edb5a831d2291c9efef4cb5f0a559cdc
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-09 19:56:36 +0200

    Fixed style of Module extension spec.

--- spec/module_extension_spec.rb
+++ spec/module_extension_spec.rb
@@ -18,28 +18,30 @@ describe Module, "attribute" do
     end
   end
 
-  context "writer extionsion" do
+  context "writer extension" do
     it "should overwrite attr_writer" do
       Module.private_methods.should include('attr_writer_with_camelize')
       Module.private_methods.should include('attr_writer_without_camelize')
     end
 
-    it "should generate setter methods" do
-      self.should_not respond_to(:foo=)
-      class Foo
-        attr_writer :foo
+    it "should generate writer methods" do
+      class FooBarBazWriterResponseTest
+        attr_writer :foo_bar_baz
       end
-      a = Foo.new
-      a.should respond_to(:foo=)
+      a = FooBarBazWriterResponseTest.new
+
+      a.should respond_to(:foo_bar_baz=)
     end
 
-    it "should generate setter methods respecting camelcased equivalents" do
-      self.should_not respond_to(:foo=)
-      class Foo
-        attr_writer :foo
+    it "should generate writer methods respecting camelcased equivalents" do
+      class FooBarBazWriterAlteredResponseTest
+        attr_writer :foo_bar_baz
       end
-      a = Foo.new
-      a.should respond_to(:foo=)
+      a = FooBarBazWriterAlteredResponseTest.new
+
+      a.should_receive(:setFooBarBaz)
+
+      a.foo_bar_baz = "boo"
     end
   end
 end
