c045f783768c0ba92c61f17c83377150e95ca66f
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-10 15:01:08 +0200

    History view now resizes columns to content.

--- kitten/main_window.rb
+++ kitten/main_window.rb
@@ -38,6 +38,8 @@ module Kitten
         @ui.historyBranchComboBox.model = @branches_model
         current_branch_index = @ui.historyBranchComboBox.find_text(repository.current_branch)
         @ui.historyBranchComboBox.current_index = current_branch_index
+
+        @ui.historyTableView.resize_columns_to_contents
       end
 
       def on_action_Open_triggered()
