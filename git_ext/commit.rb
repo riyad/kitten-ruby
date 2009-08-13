3467d0e32ecdce510258b45b3cb9b5992bb22fe8
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-13 21:40:57 +0200

    Added a method to Git::Object::Commit to get the children of a commit.

--- git_ext/commit.rb
+++ git_ext/commit.rb
@@ -6,6 +6,26 @@ module Git
         false
       end
 
+      # Returns an array with the children of this commit.
+      #
+      # The default is to only look in the history of the current branch.
+      # You can look for children in
+      #     :current : the current branch (default)
+      #     :all     : all branches
+      #     [...]    : a list of branch names
+      def children(branches = :current)
+        case branches
+        when :current: branches = [@base.current_branch]
+        when :all:     branches = @base.branches
+        else
+          branches = [branches].flatten
+        end
+
+        children_shas = @base.lib.children_of(self.sha, branches)
+        children_shas.map { |sha| Git::Object::Commit.new(@base, sha)}
+      end
+      alias_method :children_in, :children
+
       def merge?
         parents.size > 1
       end
