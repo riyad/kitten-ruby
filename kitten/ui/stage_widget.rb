36eb2518860f863bedc2f374ac88bf5270d332e1
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-19 00:00:42 +0200

    Implemented simple logic in StageWidget.

--- kitten/ui/stage_widget.rb
+++ kitten/ui/stage_widget.rb
@@ -1,9 +1,11 @@
 
+require File.join(File.dirname(__FILE__), '../models/git_unstaged_files_model')
 require File.join(File.dirname(__FILE__), 'ui_stage_widget')
 
 module Kitten
   module Ui
     class StageWidget < Qt::Widget
+      slots 'on_unstagedChangesView_clicked(const QModelIndex&)'
 
       def createUi()
         @ui = Ui_StageWidget.new
@@ -19,6 +21,22 @@ module Kitten
       end
 
       def loadModels()
+        @unstaged_files_model = Models::GitUnstagedFilesModel.new(repository, self)
+        @ui.unstagedChangesView.model = @unstaged_files_model
+      end
+
+      def reload()
+        @unstaged_files_model.reset
+      end
+
+      def on_unstagedChangesView_clicked(index)
+        status_file = @unstaged_files_model.map_to_status_file(index)
+        @ui.diffBrowser.text = if status_file
+                                  # shows old contents
+                                  status_file.blob.contents
+                                else
+                                  ''
+                                end
       end
 
       attr_accessor :repository
