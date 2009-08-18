bdbfc209341832ca3f32aabcac405bfdb1acdba0
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-18 21:53:23 +0200

    Created skeleton of StageWidget.

--- kitten/ui/stage_widget.rb
+++ kitten/ui/stage_widget.rb
@@ -4,13 +4,27 @@ require File.join(File.dirname(__FILE__), 'ui_stage_widget')
 module Kitten
   module Ui
     class StageWidget < Qt::Widget
+
+      def createUi()
+        @ui = Ui_StageWidget.new
+        @ui.setup_ui(self)
+      end
+
       def initialize(*args)
         super {}
 
-        @ui = Ui_StageWidget.new
-        @ui.setup_ui(self)
+        create_ui
 
         yield(self) if block_given?
+      end
+
+      def loadModels()
+      end
+
+      attr_accessor :repository
+      def setRepository(repo)
+        @repository = repo
+        load_models
       end
     end
   end
