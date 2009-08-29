52ec353a40ad46e42a3c2e547444c1b7b0f8f345
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-29 15:13:56 +0200

    Added actions in MainWindow.

--- kitten/ui/main_window.rb
+++ kitten/ui/main_window.rb
@@ -17,11 +17,40 @@ module Kitten
       def setupActions()
         KDE::StandardAction::quit($kapp, SLOT('quit()'), actionCollection);
 
+      # commit
+        commit_action = KDE::Action.new(self) do |a|
+          a.text = i18n("Commit")
+          a.icon = KDE::Icon.new('git-commit')
+          # TODO: make setting shortcuts work
+          #a.shortcut = Qt::CTRL + Qt::Key_Return
+        end
+        actionCollection.add_action('commit', commit_action)
+        connect(commit_action, SIGNAL('triggered(bool)'), @ui.stageWidget, SLOT('commit()'))
+
+        stage_file_action = KDE::Action.new(self) do |a|
+          a.text = i18n("Stage File to Commit")
+          a.icon = KDE::Icon.new('list-add')
+          # TODO: make setting shortcuts work
+          #a.shortcut = Qt::CTRL + Qt::Key_S
+        end
+        actionCollection.add_action('file_stage', stage_file_action)
+        connect(stage_file_action, SIGNAL('triggered(bool)'), @ui.stageWidget, SLOT('stageFile()'))
+
+        unstage_file_action = KDE::Action.new(self) do |a|
+          a.text = i18n("Unstage File from Commit")
+          a.icon = KDE::Icon.new('list-remove')
+          # TODO: make setting shortcuts work
+          #a.shortcut = Qt::CTRL + Qt::Key_U
+        end
+        actionCollection.add_action('file_unstage', unstage_file_action)
+        connect(unstage_file_action, SIGNAL('triggered(bool)'), @ui.stageWidget, SLOT('unstageFile()'))
+
+      # repository
         open_repo_action = KDE::Action.new(self) do |a|
           a.text = i18n("Open repository")
           a.icon = KDE::Icon.new('folder-open')
           # TODO: make setting shortcuts work
-          #a.shortcut = Qt::Key_F5
+          #a.shortcut = Qt::CTRL + Qt::Key_O
         end
         actionCollection.add_action('repository_open', open_repo_action)
         connect(open_repo_action, SIGNAL('triggered(bool)'), self, SLOT('open()'))
