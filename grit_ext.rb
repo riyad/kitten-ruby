c5168353adcd7ddbb2e11b792742bd8ffb29e426
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-09-18 02:18:34 +0200

    Added Repo.clone and Repo.init to Grit extensions.

--- grit_ext.rb
+++ grit_ext.rb
@@ -49,6 +49,49 @@ end
 
 module Grit
   class Repo
+    # Clones _their_repo_ into _my_repo_.
+    # Options may include:
+    # <tt>:bare => true</tt> if it should only do a bare clone
+    #
+    # Returns the Grit::Repo of the cloned repository.
+    #
+    # === Examples
+    #   # Note: that the _my_repo_ location may not exists before
+    #   Repo.clone('git://github.com/mojombo/grit.git', '~/projects/my_grit_clone')
+    #   #=> #<Grit::Repo "~/projects/my_grit_clone/.git">
+    #   Repo.clone('git://github.com/mojombo/grit.git', '~/projects/my_grit_clone.git', :bare => true)
+    #   #=> #<Grit::Repo "~/projects/my_grit_clone.git">
+    def self.clone(their_repo, my_repo, options = {})
+      repo_path = my_repo
+      bare_repo_path = options[:bare] ? repo_path : File.join(repo_path, '.git')
+
+      Grit::Git.new(bare_repo_path).clone({:bare => true}, their_repo, bare_repo_path)
+      repo = Grit::Repo.new(repo_path, :is_bare => options[:bare])
+
+      repo.git.checkout({}, 'HEAD') unless options[:bare]
+
+      repo
+    end
+
+    # Creates a fresh repository in _my_repo_.
+    #
+    # Returns the Grit::Repo of the new repository.
+    #
+    # === Examples
+    #   # Note: that the _my_repo_ location may not exists before
+    #   Repo.init('~/projects/foo')
+    #   #=> #<Grit::Repo "~/projects/foo/.git">
+    #   Repo.init('~/projects/bar.git', :bare => true)
+    #   #=> #<Grit::Repo "~/projects/bar.git">
+    def self.init(my_repo, options = {})
+      repo_path = my_repo
+      bare_repo_path = options[:bare] ? repo_path : File.join(repo_path, '.git')
+
+      Grit::GitRuby::Repository.init(bare_repo_path, options[:bare])
+
+      Grit::Repo.new(repo_path, :is_bare => options[:bare])
+    end
+
     def stage_files(*files)
       self.git.add({}, *files)
     end
