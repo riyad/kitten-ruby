3aaa810449b0b443fff93dcb7726e55dcaf4717f
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-11-09 00:02:29 +0100

    Changed diff formatting.

--- kitten/ui/diff_widget.rb
+++ kitten/ui/diff_widget.rb
@@ -12,11 +12,16 @@ module Kitten
 
       def format(unformatted_diff)
         formatted_diff = CGI.escapeHTML(unformatted_diff)
-        formatted_diff.gsub!(/^((diff|index|new|\+\+\+|---).*\n)/, '')
-        formatted_diff.gsub!(/^(\+.*)/, '<span class="add">\\1</span>')
-        formatted_diff.gsub!(/^(-.*)/, '<span class="remove">\\1</span>')
-        formatted_diff.gsub!(/^(@@.*)/, '<span class="info">\\1</span>')
-        formatted_diff.gsub!(/^(\\.*)/, '<span class="warning">\\1</span>')
+        formatted_diff.gsub!(/^(diff --git .*)/,    '<span class="header">\\1</span>')
+        formatted_diff.gsub!(/^(index .*)/,         '<span class="header info">\\1</span>')
+        formatted_diff.gsub!(/^(new file .*)/,      '<span class="header add">\\1</span>')
+        formatted_diff.gsub!(/^(deleted file .*)/,  '<span class="header remove">\\1</span>')
+        formatted_diff.gsub!(/^(\+\+\+ .*)/,        '<span class="header add">\\1</span>')
+        formatted_diff.gsub!(/^(--- .*)/,           '<span class="header remove">\\1</span>')
+        formatted_diff.gsub!(/^(\+.*)/,             '<span class="add">\\1</span>')
+        formatted_diff.gsub!(/^(-.*)/,              '<span class="remove">\\1</span>')
+        formatted_diff.gsub!(/^(@@.*)/,             '<span class="info">\\1</span>')
+        formatted_diff.gsub!(/^(\\.*)/,             '<span class="warning">\\1</span>')
         formatted_diff
         formatted_diff = <<-END
 <?xml version="1.0" ?>
@@ -26,10 +31,12 @@ module Kitten
     pre {
       font-family: 'Droid Sans Mono';
     }
-    .add { color: green; }
-    .info { color: blue; }
-    .remove { color: red; }
-    .warning { color: grey; }
+    div { display: inline; }
+    .add { color: #008C00; }
+    .header { background-color: #EEEEEE; }
+    .info { color: #0057AE; }
+    .remove { color: #BF0303; }
+    .warning { color: #FFAA00; }
   </style>
 </head>
 <body>
